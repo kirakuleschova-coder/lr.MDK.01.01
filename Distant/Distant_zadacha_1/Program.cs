1789fc23ae99a2c67da16518533cb9eb3fdb4bfb
st403 <st403@WS403-05>
2025-11-13 10:45:53 +0300

    Создала класс Распродажа телефона

--- Distant/Distant_zadacha_1/Program.cs
+++ Distant/Distant_zadacha_1/Program.cs
@@ -10,6 +10,7 @@ namespace Distant_zadacha_1
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Distant/Distant_zadacha_1/Rasprodazha telefonov.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distant_zadacha_1
+{
+    internal class Rasprodazha_telefonov
+    {
+        public DateTime Date { get; set; }     // Дата продажи (метка времени)
+        public string telefonModel { get; set; } // Модель телефона
+        public int kolichestvo { get; set; }      // Количество проданных единиц
+        public double Price { get; set; }      // Цена за единицу для клиента
+        public double zatrat { get; set; }       // Затраты магазина на телефоны
+
+        // Вычисление выгоды магазина
+        public double Profit => (Price - zatrat) * kolichestvo;
+
+        // Вычисление дохода магазина
+        public double Revenue => Price * kolichestvo;
+
+    }
+}
