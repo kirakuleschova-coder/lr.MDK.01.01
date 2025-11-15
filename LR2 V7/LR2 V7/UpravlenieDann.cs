1d6d46ed443facc276016ee104a7f72f1406e0d5
qqniqq <mstitel2025@gmail.com>
2025-11-15 13:05:56 +0300

    Не успела доделать, в процессе

---
+++ LR2 V7/LR2 V7/UpravlenieDann.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_V7
+{
+   public struct UpravlenieDann
+    {
+        public double PriceHistory; //Запись об изменении цен
+        public int Category; //Категории товаров
+        public double Product ; //Товар с историей цен
+    }
+}
