0631759d66c35371c928f1cf50434bad1c389693
Daniil5903 <danilka7518@gmail.com>
2025-11-22 10:43:34 +0300

    Создала класс Car

---
+++ NFS_Console/Infested/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infested
+{
+    internal class Car
+    {
+        private string model_;
+        private double speed_;
+        //Наименование
+
+        public string GetModel()
+        {
+            return model_;
+        }
+        public void SetModel(string model)
+        {
+            model_ = model;
+        }
+        public double GetSpeed()
+        {
+            return speed_;
+        }
+       public void SetSpeed (double speed)
+        {
+            speed_ = speed;
+        }
+
+    }
+}
--- NFS_Console/Infested/Program.cs
+++ NFS_Console/Infested/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,33 @@ namespace Infested
 {
     internal class Program
     {
+        //Функция
+        static void PrintModel(Car model)
+        {
+            Console.WriteLine(model.GetModel());
+        }
         static void Main(string[] args)
         {
-            
+            //Первый автобобил
+            Car avtomobil = new Car();
+
+            avtomobil.SetModel("Lada");
+            avtomobil.SetSpeed(150);
+            //Console.WriteLine(avtomobil.GetModel() + "" + avtomobil.GetSpeed());
+            PrintModel(avtomobil);
+
+            //Второй автомобил
+            Car avtomobil1 = new Car();
+            avtomobil1.SetModel("Mersedez");
+            avtomobil1.SetSpeed(300);
+            //Console.WriteLine(avtomobil.GetModel() + "" + avtomobil.GetSpeed());
+
+            PrintModel(avtomobil1);
+
+            List<Car> avtomobili = new List<Car>();
+            avtomobili.Add(avtomobil);
+            avtomobili.Add(avtomobil1);
+
         }
     }
 }
