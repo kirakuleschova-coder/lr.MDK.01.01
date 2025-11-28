669288673b010d1dbe65e63ebc53314ab19c6945
Кира <Kirakuleschova@gmail.com>
2025-11-28 17:53:15 +0300

    Решила задачу2

---
+++ Distant DZ/Zadanie_2/DistantZadanie2/MatrixOfIntegers.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistantZadanie2
+{
+    internal class MatrixOfIntegers
+    {
+        // Приватные поля для хранения состояния матрицы
+        private int rows_;     // Количество строк в матрице
+        private int cols_;     // Количество столбцов в матрице
+        private int[] data_;   // Одномерный массив для хранения элементов матрицы в построчном порядке
+
+        // Количество строк
+        public int GetRows()
+        {
+            return rows_;
+        }
+        public void SetRows(int rows)
+        {
+            rows_ = rows;
+        }
+
+        // Количество столбцов
+        public int GetCols()
+        {
+            return cols_;
+        }
+        public void SetCols(int cols)
+        {
+            cols_ = cols;
+        }
+
+        // Данные матрицы
+        public int[] GetData()
+        {
+            return data_;
+        }
+        public void SetData(int[] data)
+        {
+            data_ = data;
+        }
+
+        // Получить значение из ячейки
+        public int GetValue(int row, int col)
+        {
+            return data_[row * cols_ + col]; // Вычисление индекса в одномерном массиве
+        }
+
+        // Установить значение в ячейку
+        public void SetValue(int row, int col, int value)
+        {
+            data_[row * cols_ + col] = value;  // Вычисление индекса и записывает значение
+        }
+
+        // Получить всю строку
+        public int[] GetRow(int row)
+        {
+            int[] result = new int[cols_];    // Создает массив длинной в кол-во столбцов
+            for (int j = 0; j < cols_; j++)
+            {
+                result[j] = GetValue(row, j); // Копирует элементы строки в массив
+            }
+            return result;
+        }
+
+        // Получить весь столбец
+        public int[] GetColumn(int col)
+        {
+            int[] result = new int[rows_];    // Создает массив длинной в кол-во строк
+            for (int i = 0; i < rows_; i++)
+            {
+                result[i] = GetValue(i, col); // Копирует элементы столбца в массив
+            }
+            return result;
+        }
+
+        // Проверить квадратная ли матрица
+        public bool IsSquare()
+        {
+            return rows_ == cols_;
+        }
+    }
+}
---
+++ Distant DZ/Zadanie_2/DistantZadanie2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistantZadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Разработать класс матрица целых чисел, операции возможность показать количество строк
+            // столбцов матрицы, указать значения определенной строки или столбца, указать значение ячейки,
+            // получить значение ячейки, для квадратной матрицы прочитать определитель.
+            // Реализовать функцию умножения матриц
+
+            // Создание матрицы
+            MatrixOfIntegers matrix = new MatrixOfIntegers();
+            matrix.SetRows(3);
+            matrix.SetCols(3);
+            matrix.SetData(new int[9]);
+
+            // Заполнение матрицы циклом
+            int value = 1;
+            for (int i = 0; i < matrix.GetRows(); i++)
+            {
+                for (int j = 0; j < matrix.GetCols(); j++)
+                {
+                    matrix.SetValue(i, j, value);
+                    value++;
+                }
+            }
+            Console.WriteLine("------ ИНФОРМАЦИЯ О МАТРИЦЕ ------");
+
+            // 1. Показ матрицы
+            Console.WriteLine("1. Матрица 3x3:");
+            for (int i = 0; i < matrix.GetRows(); i++)          // Цикл по всем строкам матрицы
+            {
+                for (int j = 0; j < matrix.GetCols(); j++)      // Цикл по всем столбцам матрицы
+                {
+                    Console.Write(matrix.GetValue(i, j) + " ");
+                }
+                Console.WriteLine();
+            }
+            // 2. Показ количество строк и столбцов
+            Console.WriteLine("2. Размер матрицы:");
+            Console.WriteLine($"Количество строк: {matrix.GetRows()}");
+            Console.WriteLine($"Количество столбцов: {matrix.GetCols()}");
+
+            // 3. Вопросы о конкретной строке
+            Console.Write("3. Введите номер строки для показа (0-2): ");
+            int rowNumber = int.Parse(Console.ReadLine());         // Преобразуем ввод пользователя в число
+            if (rowNumber >= 0 && rowNumber < matrix.GetRows())    // Проверка, что номер строки находится в диапазоне
+            {
+                int[] row = matrix.GetRow(rowNumber);   // Поллучение массива с элементами строки, которую мы указали
+                Console.WriteLine($"Строка {rowNumber}: " + string.Join(", ", row));
+            }
+            else
+            {
+                Console.WriteLine("Неверный номер строки!");
+            }
+
+            // 4. Вопросы о конкретном столбце
+            Console.Write("4. Введите номер столбца для показа (0-2): ");
+            int colNumber = int.Parse(Console.ReadLine());
+
+            if (colNumber >= 0 && colNumber < matrix.GetCols())
+            {
+                int[] column = matrix.GetColumn(colNumber);
+                Console.WriteLine($"Столбец {colNumber}: " + string.Join(", ", column));
+            }
+            else
+            {
+                Console.WriteLine("Неверный номер столбца!");
+            }
+
+            // 5. Вопросы о кенкретной ячейке
+            Console.Write("5. Введите номер строки для ячейки (0-2): ");
+            int cellRow = int.Parse(Console.ReadLine());
+            Console.Write("Введите номер столбца для ячейки (0-2): ");
+            int cellCol = int.Parse(Console.ReadLine());
+
+            if (cellRow >= 0 && cellRow < matrix.GetRows() && cellCol >= 0 && cellCol < matrix.GetCols())
+            {
+                Console.WriteLine($"Ячейка [{cellRow},{cellCol}]: {matrix.GetValue(cellRow, cellCol)}");
+            }
+            else
+            {
+                Console.WriteLine("Неверные координаты ячейки!");
+            }
+
+            // 6. Проверка квадратная ли матрица
+            Console.WriteLine("6. Проверка на квадратность:");
+            if (matrix.IsSquare())
+            {
+                Console.WriteLine("Матрица квадратная");
+            }
+            else
+            {
+                Console.WriteLine("Матрица не квадратная");
+            }
+            Console.WriteLine("------ ВСЕ ОПЕРАЦИИ ЗАВЕРШЕНЫ ------");
+        }
+    }
+}
