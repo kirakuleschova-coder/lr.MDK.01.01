92da4c9dd9b11afa12bbee2f329a9316e6122670
mariya-koz <mkozyreva868@gmail.com>
2025-12-02 10:57:58 +0300

    Рассписала значение строк

--- LR1/zadanie1/Program.cs
+++ LR1/zadanie1/Program.cs
@@ -10,26 +10,26 @@ namespace zadanie1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите количество чисел в массиве: ");
-            int n = int.Parse(Console.ReadLine());
-            int[] array = new int[n];
-            int EvenCount = 0;
+            Console.WriteLine("Введите количество чисел в массиве: ");// Вывод приглашения для ввода количества чисел
+            int n = int.Parse(Console.ReadLine());// Чтение ввода пользователя и преобразование строки в целое число
+            int[] array = new int[n];// Создание массива целых чисел размером n
+            int EvenCount = 0;// Переменные для подсчёта чётных и нечётных чисел
             int OddCount = 0;
-            Console.WriteLine($"Введите " + n + " целых чисел: ");
-            for (int i = 0; i < n; i++)
+            Console.WriteLine($"Введите " + n + " целых чисел: ");// Вывод приглашения для ввода n чисел
+            for (int i = 0; i < n; i++)// Цикл для ввода n чисел
             {
-                array[i] = int.Parse(Console.ReadLine());
-                if (array[i] % 2 == 0)
+                array[i] = int.Parse(Console.ReadLine());// Чтение и преобразование введённого числа
+                if (array[i] % 2 == 0)// Проверка чётности числа
                 {
-                    EvenCount++;
+                    EvenCount++;  // Увеличение счётчика чётных чисел
                 }
                 else
                 {
-                    OddCount++;
+                    OddCount++; // Увеличение счётчика нечётных чисел
                 }
             }
-            Console.WriteLine($"Количество четных элементов: " + EvenCount);
-            Console.WriteLine($"Количество нечетных элементов: " + OddCount);
+            Console.WriteLine($"Количество четных элементов: " + EvenCount);// Вывод результатов
+            Console.WriteLine($"Количество нечетных элементов: " + OddCount);// Вывод результатов
         }
     }
 }
