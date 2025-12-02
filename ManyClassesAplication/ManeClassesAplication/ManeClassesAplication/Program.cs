16244e1b79179c3173e6ac1c23a06d0eaab56466
mariya-koz <mkozyreva868@gmail.com>
2025-12-02 08:34:19 +0300

    Создала проект для дальнейшего отчёта

--- ManyClassesAplication/ManeClassesAplication/ManeClassesAplication/Program.cs
+++ ManyClassesAplication/ManeClassesAplication/ManeClassesAplication/Program.cs
@@ -6,11 +6,44 @@ using System.Threading.Tasks;
 
 namespace ManeClassesAplication
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Product mers = new Product();
+            mers.SetName("Мерседес");
+            mers.SetPrice(1000000);
 
+            Product yaz = new Product();
+            yaz.SetName("Уазик");
+            yaz.SetPrice(10000000);
+
+            Storage storage = new Storage();
+            storage.SetId(123);
+            storage.SetLocation("Торжок, Ул. Студенческая 3");
+            storage.SetProductQuantity(mers, 2);
+            storage.SetProductQuantity(yaz, 5);
+
+            storage.GetProductQuantity();
+            storage.CalculateMoney();
+
+            Console.WriteLine();
+
+            Report report = new Report();
+
+            ReportRow row1 = new ReportRow();
+            row1.Product = "Mers";
+            row1.Quantity = 500;
+            row1.Price = 100;
+
+            ReportRow row2 = new ReportRow();
+            row2.Product = "Vaz";
+            row2.Quantity = 100;
+            row2.Price = 5192;
+
+            Console.WriteLine("Содержимое отчета:");
+            Console.WriteLine(report);
+            report.CalculateSum();
         }
     }
 }
---
+++ ManyClassesAplication/ManeClassesAplication/ManeClassesAplication/Report.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManeClassesAplication
+{
+    public class Report
+    {
+        private List<ReportRow> rows_ = new List<ReportRow>();
+        public void AddRecord(ReportRow row)
+        {
+            rows_.Add(row);
+        }
+        public string ConvertReportToString(ReportRow row)
+        {
+            return row.Product + ";" + row.Quantity + ";" + row.Price;
+        }
+        public string ConvertAllToString()
+        {
+            if (rows_.Count == 0)
+                return "Отчет пуст";
+            StringBuilder sb = new StringBuilder();
+            foreach (ReportRow row in rows_)
+            {
+                sb.AppendLine(ConvertReportToString(row));
+            }
+            return sb.ToString();
+        }
+        public void CalculateSum()
+        {
+            double sum = 0;
+            foreach (ReportRow row in rows_)
+            {
+                sum += row.Quantity * row.Price;
+            }
+            Console.WriteLine("Сумма  - " + sum);
+        }
+    }
+}
