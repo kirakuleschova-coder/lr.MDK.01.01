72308f8a9f42f04658b5bc7a2e36e001a339f990
mariya-koz <mkozyreva868@gmail.com>
2025-12-02 10:56:04 +0300

    Решила задание 1

---
+++ LR1/zadanie1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел в массиве: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int EvenCount = 0;
+            int OddCount = 0;
+            Console.WriteLine($"Введите " + n + " целых чисел: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                if (array[i] % 2 == 0)
+                {
+                    EvenCount++;
+                }
+                else
+                {
+                    OddCount++;
+                }
+            }
+            Console.WriteLine($"Количество четных элементов: " + EvenCount);
+            Console.WriteLine($"Количество нечетных элементов: " + OddCount);
+        }
+    }
+}
