fff85ba4212a2b563fbc6aaf2320352fecc3a8d6
mariya-koz <mkozyreva868@gmail.com>
2025-12-02 11:01:15 +0300

    Решила задачу 2

---
+++ LR1/zadanie2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3000];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10, 10000);
+            }
+            Console.WriteLine("Исходные данные: ");
+            Console.WriteLine("[" + string.Join(", ", array) + "]");
+            List<int> Summa = new List<int>();
+            for (int i = 0; i < array.Length - 2; i += 2)
+            {
+                int sum = array[i] + array[i + 2];
+                Summa.Add(sum);
+            }
+            Console.WriteLine("Результат: ");
+            Console.WriteLine("[" + string.Join(", ", Summa) + "]");
+        }
+    }
+}
