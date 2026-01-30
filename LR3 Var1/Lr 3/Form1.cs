13c8e78be6592f305774d4bb375c1b7922dc5e93
Fa1tloy <baranovdaniil534@gmail.com>
2026-01-30 11:15:04 +0300

    В процессе выполнения задачи. Проихзошла ошибка в списке

---
+++ LR3 Var1/Lr 3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lr_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<Tovar> Tovars = new List<Tovar>();
+            Tovars.Add(new Tovar("Сталь", 230, "Германия", "23"));
+            Tovars.Add(new Tovar("Чугун", 610, "Россия", "73"));
+            Tovars.Add(new Tovar("Полиэтилен", 90, "Испания", "56"));
+            Tovars.Add(new Tovar("Стеатитовая керамика", 570, "Индия", "30"));
+            Tovars.Add(new Tovar("Металлические композиты", 1350, "Россия", "78"));
+        }
+    }
+}
---
+++ LR3 Var1/Lr 3/Tovar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr_3
+{
+    internal class Tovar
+    {
+        private string name_;
+        private string postavshik_;
+        private string proizvoditel_;
+        private int summa_;
+        private double nomeclaturnGroup_;
+        private string foto_;
+
+        public string Name
+        {
+            get { return name_; }
+        }
+
+            public string Symma
+        {
+            get { return Convert.ToString(summa_); }
+        }
+
+
+            public string NomeclaturnGroup_
+        {
+            get { return Convert.ToString(nomeclaturnGroup_); }
+        }
+
+
+        public string Postavshik
+        {
+            get {return Convert.ToString(postavshik_); }
+        }
+
+        public string Proizvoditel
+        {
+            get { return Convert.ToString(proizvoditel_); }
+        }
+
+        public string Foto
+        {
+            get { return foto_; }
+        }
+
+    }
+}
