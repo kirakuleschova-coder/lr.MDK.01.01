e5b5566c7e928ff5039eab6cb45ccc33d3067d79
Fa1tloy <baranovdaniil534@gmail.com>
2026-01-30 09:36:01 +0300

    В процессе

---
+++ TestingModul/TestingModul/AnalisAvto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoArend
+{
+    public class AnalisAvto
+    {
+        ///Сколько автомобилей за день
+        public string HuwManyCarsAreRentedPerDay()
+        {
+            Console.Write("Введите марку машины: ");
+            string stamp = Console.ReadLine();
+
+
+        }
+    }
+}
