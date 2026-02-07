e433e23a55363cbde05b49664dea23afef458ea9
Andrey-Shelgunov <shelgunova3@gmail.com>
2026-02-07 12:29:04 +0300

    Сщздала конструктор

--- Lr3 Var1/Var1 LR3/Var1 LR3/Form1.Designer.cs
+++ Lr3 Var1/Var1 LR3/Var1 LR3/Form1.Designer.cs
@@ -28,21 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.SupplierLabel = new System.Windows.Forms.Label();
+            this.NomenclatureLabel = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 357);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameLabel.Location = new System.Drawing.Point(314, 50);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(154, 18);
+            this.NameLabel.TabIndex = 1;
+            this.NameLabel.Text = "Название материала";
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CostLabel.Location = new System.Drawing.Point(314, 145);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(86, 18);
+            this.CostLabel.TabIndex = 2;
+            this.CostLabel.Text = "Стоимость";
+            // 
+            // SupplierLabel
+            // 
+            this.SupplierLabel.AutoSize = true;
+            this.SupplierLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SupplierLabel.Location = new System.Drawing.Point(314, 194);
+            this.SupplierLabel.Name = "SupplierLabel";
+            this.SupplierLabel.Size = new System.Drawing.Size(86, 18);
+            this.SupplierLabel.TabIndex = 3;
+            this.SupplierLabel.Text = "Поставщик";
+            // 
+            // NomenclatureLabel
+            // 
+            this.NomenclatureLabel.AutoSize = true;
+            this.NomenclatureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NomenclatureLabel.Location = new System.Drawing.Point(314, 247);
+            this.NomenclatureLabel.Name = "NomenclatureLabel";
+            this.NomenclatureLabel.Size = new System.Drawing.Size(175, 18);
+            this.NomenclatureLabel.TabIndex = 4;
+            this.NomenclatureLabel.Text = "Номенклатурная группа";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(596, 50);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.NomenclatureLabel);
+            this.Controls.Add(this.SupplierLabel);
+            this.Controls.Add(this.CostLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "v";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label SupplierLabel;
+        private System.Windows.Forms.Label NomenclatureLabel;
+        private System.Windows.Forms.ComboBox comboBox;
     }
 }
 
